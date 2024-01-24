5be6918cd1aceb8649005c63d0102ae803441996
virangaj <virangapasindu4@gmail.com>
2024-01-24 20:39:38 +0530

    add cors

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const cors = require('cors');
+
+
 const port = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 io.on("connection", (socket) => {
   console.log("a user connected");
