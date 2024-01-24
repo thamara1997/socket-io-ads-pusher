7c291b7ea5283d533c066f5e3751d70a4f5dcc0f
virangaj <virangapasindu4@gmail.com>
2024-01-24 20:29:39 +0530

    add socker

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const port = 3000;
+
+app.use(express.json());
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
+
+// Define the POST endpoint
+app.post("/post-ad", (req, res) => {
+  const data = req.body;
+  console.log("Received data:", data);
+  io.emit("broadcastData", data);
+
+  res.status(200).send("Data received and broadcasted");
+});
+
+http.listen(port, () => {
+  console.log("listening on *:", port);
+});
